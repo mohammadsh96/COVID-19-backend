649982d3171190c53e43827c141f90cbd793f692
outsidett <outside@pathianlabs.com>
2022-12-17 18:12:53 +0000

    try http createServer

--- src/server.js
+++ src/server.js
@@ -3,22 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+var http = require('http');
+var server = http.createServer(app);
 const notFoundHandler = require("./handlers/404");
 const errorHandler = require("./handlers/500");
 const authRouter = require("../src/routes/usersRouter");
 const recordsRouter = require("./routes/recordsRouter");
-var http = require('http');
-
-var server = http.createServer(app);
-
-app.get('/', function(req, res) {
-    res.send("Hello World!");
-});
-
-// server.listen(3000, 'localhost');
-// server.on('listening', function() {
-//     console.log('Express server started on port %s at %s', server.address().port, server.address().address);
-// })
 app.use(express.json());
 app.use(cors());
 app.use(authRouter);
@@ -30,16 +20,22 @@ app.use(errorHandler);
 function start(PORT) {
   try {
     server.listen(PORT, '0.0.0.0')
-    // app.listen(PORT, () => {
-    //   console.log(`Listen and Running on port ${PORT}`);
-    // });
-  } catch (err) {
+     } catch (err) {
     console.log(err);
   }
 }
 server.on('listening', function() {
     console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 })
+server.on('error', (e) => {
+    if (e.code === 'EADDRINUSE') {
+      console.log('Address in use, retrying...');
+      setTimeout(() => {
+        server.close();
+        server.listen(PORT, HOST);
+      }, 1000);
+    }
+  });
 module.exports = {
   app: app,
   start: start,
